67f8bc99a9fb9db33acdd21b117b38d7497c16c9
unknown <mateusrisso@yahoo.com.br>
2025-12-25 23:45:52 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,58 @@
+# Frontend Mentor - Time tracking dashboard solution
+
+This is a solution to the [Time tracking dashboard challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/time-tracking-dashboard-UIQ7167Jw). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+  - [Continued development](#continued-development)
+- [Author](#author)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the site depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Switch between viewing Daily, Weekly, and Monthly stats
+
+### Screenshot
+
+![Screenshot Mobile](screenshot-mobile.png)
+![Screenshot Desktop](screenshot-desktop.png)
+
+### Links
+
+- Solution URL: [Add solution URL here](https://your-solution-url.com)
+- Live Site URL: [https://mateusrissodiblasio.github.io/time-tracking-dashboard-solution](https://mateusrissodiblasio.github.io/time-tracking-dashboard-solution)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- Mobile-first workflow
+
+### What I learned
+
+Manipulating Json file data to switch info stats on button clicks.
+
+### Continued development
+
+Using what I learned on this challenge has inspired me to create some projects of my own with Json data.
+
+## Author
+
+- GitHub - [Mateus Risso Di Blasio](https://github.com/MateusRissoDiBlasio)
+- Frontend Mentor - [@MateusRissoDiBlasio](https://www.frontendmentor.io/profile/MateusRissoDiBlasio)
---
+++ index.js
@@ -0,0 +1,110 @@
+fetch('data.json')
+    .then(response => {
+        
+        if (!response.ok) {
+            throw new Error('Network response was not ok ' + response.statusText);
+        }
+        return response.json();
+    })
+    .then(data => {
+        createCards(data);
+    })
+    .catch(error => {
+        console.error('There has been a problem with your fetch operation:', error);
+    });
+
+    function createCards(cardData) {
+    const container = document.getElementById('reportCards');
+
+    cardData.forEach(cardInfo => {
+
+        const fullCard = document.createElement('div');
+        fullCard.classList.add('reportDataCardContainer')
+        const card = document.createElement('div');
+        card.classList.add('reportCard', cardInfo.title.replace(/\s+/g, '-'));
+        const img = document.createElement('img');
+        img.src = './images/icon-' + cardInfo.title.replace(/\s+/g, '-') + '.svg';
+        const reportDataCard = document.createElement('div');
+        reportDataCard.classList.add('reportData');
+        const reportDataCardContainer = document.createElement('div');
+        reportDataCard.appendChild(reportDataCardContainer);
+        const elypsesImg = document.createElement('img');
+        elypsesImg.src = './images/icon-ellipsis.svg';
+        const title = document.createElement('p');
+        title.textContent = cardInfo.title;
+
+        const reportDataCardContainerHours = document.createElement('div');
+        
+        var actualData = document.createElement('h1');
+        actualData.classList.add('currentData');
+        actualData.textContent = cardInfo.timeframes.daily.current + 'hrs';
+        var passedData = document.createElement('p');
+        passedData.classList.add('previousData');
+        passedData.classList.add('previous');
+        passedData.textContent = 'Last Day - ' + cardInfo.timeframes.daily.previous + 'hrs'
+
+        reportDataCardContainerHours.appendChild(actualData);
+        reportDataCardContainerHours.appendChild(passedData);
+        reportDataCard.appendChild(reportDataCardContainerHours);
+        reportDataCardContainer.appendChild(title);
+        reportDataCardContainer.appendChild(elypsesImg);
+        card.appendChild(img);
+        fullCard.appendChild(card);
+        fullCard.appendChild(reportDataCard);
+        container.appendChild(fullCard)
+
+        const dailyBtn = document.getElementById('daily');
+        const weeklyBtn = document.getElementById('weekly');
+        const monthlyBtn = document.getElementById('monthly');
+        dailyBtn.classList.add('active');
+
+        weeklyBtn.addEventListener('click', function() {
+            weeklyBtn.classList.add('active');
+            dailyBtn.classList.remove('active');
+            monthlyBtn.classList.remove('active');
+            
+            const actualData = document.querySelectorAll('.currentData');
+            for (let i = 0; i < actualData.length; i++){
+                actualData[i].textContent = cardData[i].timeframes.weekly.current + 'hrs';
+            }
+            
+            const passedData = document.querySelectorAll('.previousData');
+            for (let i = 0; i < passedData.length; i++){
+                passedData[i].textContent = 'Last Week - ' + cardData[i].timeframes.weekly.previous + 'hrs';
+            }
+            
+        });
+
+        monthlyBtn.addEventListener('click', function() {
+            monthlyBtn.classList.add('active');
+            weeklyBtn.classList.remove('active');
+            dailyBtn.classList.remove('active');
+
+            const actualData = document.querySelectorAll('.currentData');
+            for (let i = 0; i < actualData.length; i++){
+                actualData[i].textContent = cardData[i].timeframes.monthly.current + 'hrs';
+            }
+            
+            const passedData = document.querySelectorAll('.previousData');
+            for (let i = 0; i < passedData.length; i++){
+                passedData[i].textContent = 'Last Month - ' + cardData[i].timeframes.monthly.previous + 'hrs';
+            }
+        });
+        
+        dailyBtn.addEventListener('click', function() {
+            dailyBtn.classList.add('active');
+            monthlyBtn.classList.remove('active');
+            weeklyBtn.classList.remove('active');
+
+            const actualData = document.querySelectorAll('.currentData');
+            for (let i = 0; i < actualData.length; i++){
+                actualData[i].textContent = cardData[i].timeframes.daily.current + 'hrs';
+            }
+            
+            const passedData = document.querySelectorAll('.previousData');
+            for (let i = 0; i < passedData.length; i++){
+                passedData[i].textContent = 'Last Day - ' + cardData[i].timeframes.daily.previous + 'hrs';
+            }
+        }); 
+    });   
+}
